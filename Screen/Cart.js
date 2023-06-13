a4583e5546e4c8fb6e540dc291853bf052116c8f
Huyfpl <94282645+huyfpl@users.noreply.github.com>
2023-06-13 13:13:42 +0700

    gio hang

--- Screen/Cart.js
+++ Screen/Cart.js
@@ -1,15 +1,171 @@
 import React from "react";
-import { View,Text } from "react-native";
-export default class Cart extends  React.Component{
-    static navigationOptions = {
-        title: 'Cart',
-      };
-    render(){
-        return(
-            <View>
-          
-                <Text>Cart</Text>
-            </View>
-        )
+import { View, Text, Image, StyleSheet, TouchableOpacity, FlatList } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { API_LISTGIOHANG_USER_ID } from "../helpers/api";
+import axios from 'axios';
+import Carousel from 'react-native-snap-carousel';
+
+export default class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: null,
+      giohang: null,
+      danhMuc: {}, // Lưu trữ danh sách danh mục
+    };
+  }
+
+  componentDidMount() {
+    this.getUserData();
+  }
+
+  async getUserData() {
+    try {
+      const userId = await AsyncStorage.getItem('userId');
+      this.setState({ userId: userId }, () => {
+        this.fetchUserData();
+      });
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  async fetchUserData() {
+    const { userId } = this.state;
+    try {
+      const response = await axios.get(`${API_LISTGIOHANG_USER_ID}/${userId}`);
+      const data = response.data;
+      const giohang = data.products;
+      const danhMuc = {};
+
+      giohang.forEach(item => {
+        if (!danhMuc[item.ten_danhmuc]) {
+          danhMuc[item.ten_danhmuc] = [];
+        }
+        danhMuc[item.ten_danhmuc].push(item);
+      });
+
+      this.setState({ giohang, danhMuc });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  handleIncreaseQuantity = (itemIndex) => {
+    // Logic to increase quantity for the product at itemIndex
+  }
+
+  handleDecreaseQuantity = (itemIndex) => {
+    // Logic to decrease quantity for the product at itemIndex
+  }
+
+  renderProductItem = ({ item, index }) => {
+    return (
+      <View style={styles.productItem}>
+        <View style={styles.productInfo}>
+          <Image source={{ uri: item.anhSP }} style={styles.productImage} />
+          <View style={styles.productDetails}>
+            <Text style={styles.productName}>{item.tenSP}</Text>
+            <View style={styles.quantityContainer}>
+              <TouchableOpacity onPress={() => this.handleDecreaseQuantity(index)}>
+                <Text style={styles.quantityButton}>-</Text>
+              </TouchableOpacity>
+              <Text style={styles.quantityText}>{item.soluong}</Text>
+              <TouchableOpacity onPress={() => this.handleIncreaseQuantity(index)}>
+                <Text style={styles.quantityButton}>+</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    const { danhMuc } = this.state;
+    return (
+      <View style={styles.container}>
+        {Object.keys(danhMuc).length > 0 ? (
+          Object.keys(danhMuc).map(categoryName => (
+            <View key={categoryName} style={styles.carouselContainer}>
+              <Text style={styles.title}>{categoryName}</Text>
+              <Carousel
+              data={danhMuc[categoryName]}
+              renderItem={this.renderProductItem}
+              sliderHeight={100} 
+              itemHeight={150} 
+vertical={true} 
+              contentContainerStyle={styles.carouselContentContainer}
+            />
+            </View>
+          ))
+        ) : (
+          <Text style={styles.emptyCartText}>No user data available</Text>
+        )}
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  carouselContainer:{
+  backgroundColor:'red',
+  },
+  carouselContentContainer:{
+    flexGrow: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  productItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  productInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  productImage: {
+    width: 100,
+    height: 100,
+    marginRight: 10,
+  },
+  productDetails: {
+    flex: 1,
+  },
+  productName: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  quantityContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quantityButton: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 4,
+    marginHorizontal: 5,
+  },
+  quantityText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+  },
+  emptyCartText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
--- helpers/api.js
+++ helpers/api.js
@@ -1,4 +1,4 @@
-export const API_URL='http://192.168.0.103:3000/api'
+export const API_URL='http://192.168.0.105:3000/api'
 
 export const API_LIST_SANPHAM=API_URL+'/listsanpham'
 export const API_LOGIN=API_URL + '/listuser'
@@ -6,4 +6,5 @@ export const API_SIGNUP=API_URL + '/register-user'
 export const API_LIST_DANHMUC=API_URL + '/danhmuc'// lấy danhmuc
 export const API_LIST_SANPHAM_DANHMUC=API_URL + '/listsanpham-danhmuc';// lấy danhmuc theo id
 export const API_LIST_USER_ID=API_URL + '/listuser-id';// lấy user theo id
+export const API_LISTGIOHANG_USER_ID=API_URL + '/giohang';// lấy user theo id
 
